0db461b1a3dc00e9ddb93bef9e887a60fc646e0e
Florencia5 <floordarosa2002@gmail.com>
2023-03-14 19:48:14 -0300

    se agegra tp

---
+++ Equipo.ts
@@ -0,0 +1,22 @@
+class Equipo {
+public id: string = "";
+public descripcion: string= "";
+public fechaFabricacion: Date;
+public fechaInstalacion: Date;
+ 
+
+
+
+}
+export default { Equipo }
+
+/*
+id: string
+-descripcion: string
+-fechaFabricacion: date
+-fechaInstalacion:date  
++agregar_equipo()
++leer_eqipo()
++eliminar_equipo()
++editar_equipo()
+ */
